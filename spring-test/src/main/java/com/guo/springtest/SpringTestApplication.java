9f23ce60f50170759c005c9bdb19ba5e8ecbf35f
guoshufeng <123456ab>
2020-07-02 01:22:33 +0800

    spring-test 单数据库连接

--- spring-test/src/main/java/com/guo/springtest/SpringTestApplication.java
+++ spring-test/src/main/java/com/guo/springtest/SpringTestApplication.java
@@ -1,13 +1,41 @@
 package com.guo.springtest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+@Slf4j
 @SpringBootApplication
-public class SpringTestApplication {
-
+public class SpringTestApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		showConnection();
+		//showData();
+	}
+
+	private void showConnection() throws SQLException {
+		log.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
+	}
+
+	private void showData() {
+		jdbcTemplate.queryForList("SELECT * FROM FOO")
+				.forEach(row -> log.info(row.toString()));
+	}
 }
